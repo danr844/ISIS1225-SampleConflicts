54ca2df287e12603d25c9d8d62b155586ae5e90a
Daniel Angel <da.angelf@uniandes.edu.co>
2021-02-05 03:08:03 -0500

    commit

--- App/model.py
+++ App/model.py
@@ -71,12 +71,6 @@ def createTagList():
 
 def addBookTags(booktagsfile):
     # TO-DO: Modificación del Est-1 y Est-2 en el Lab 2
-<<<<<<< HEAD
     bts = lt.newList(datastructure='SINGLE_LINKED',
                        filename=booktagsfile)
     return bts
-=======
-    booktags = lt.newList(datastructure='SINGLE_LINKED',
-        filename=booktagsfile)
-    return booktags
->>>>>>> origin/Est-1
