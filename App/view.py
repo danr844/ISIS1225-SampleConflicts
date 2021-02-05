13e04e9c9b8ae91a77864cd2e3b889bc19c43448
Daniel Alejandro Angel Fuertes <da.angelf@uniandes.edu.co>
2021-02-05 02:16:27 -0500

    second commit 4b point

--- App/view.py
+++ App/view.py
@@ -43,7 +43,6 @@ def printMenu():
     print("1- Cargar Libros")
     print("2- Cargar Tags")
     # TO-DO: Modificación del Est-1 en el Lab 2
-    print("3- Carga Book-Tags!")
     print("0- Salir")
 
 
@@ -82,14 +81,17 @@ while True:
 
 
         # TO-DO: Modificación del Est-2 en el Lab 2
+        print('Ultimo libro cargado: ' + str(lt.lastElement(books)))
 
     elif int(inputs[0]) == 2:
         print("Cargando información de tags....")
         tags = loadTags()
         print('Total de tags cargados: ' + str(lt.size(tags)))
-    elif True:
+    elif int(inputs[0]) == 3:
         # TO-DO: Modificación del Est-2 en el Lab 2
-        pass
+        print("Cargando información de Book-Tags....")
+        booktags = loadBookTags()
+        print('Total de Book-Tags cargados: ' + str(lt.size(booktags)))
 
     else:
         sys.exit(0)
